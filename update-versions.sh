37cfc44206a47289170c9c546d04d52156947607
engels74 <141435164+engels74@users.noreply.github.com>
2026-04-20 19:51:12 +0200

    chore: track release tags instead of main commit sha

--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -exuo pipefail
 
-version=$(curl -fsSL "https://api.github.com/repos/engels74/otpravkarr/commits/main" | jq -re '.sha')
+version=$(curl -fsSL "https://api.github.com/repos/engels74/otpravkarr/tags" | jq -re '.[0].name')
 json=$(cat meta.json)
 jq --sort-keys \
-    --arg version "${version}" \
+    --arg version "${version//v/}" \
     '.version = $version' <<< "${json}" | tee meta.json
